1ac0d7aca80bb189bcf85c160a21565f2b1c4dcb
okkyAdiansyah <okkyadiansyah.0610@gmail.com>
2024-10-07 23:58:04 +0700

    Project Init

---
+++ function.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Esquire Author-Editor Live Collaborative
+ * Version: 1.0
+ * Author: Okky Adiansyah
+ * Description: Saved the current edited draft before publishing, show which part that has been edited by the author, documented every changes made into the article to help on any legal action, enable live editing functionality and centralize publishing workflow into Author-Editor Collaborative Platform 
+ */
